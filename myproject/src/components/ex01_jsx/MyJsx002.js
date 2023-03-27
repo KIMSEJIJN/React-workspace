934bda7dca30fe902ca89b25e02515cf7ae732b2
ksjin514 <ksjin514@gmail.com>
2023-03-27 17:42:04 +0900

    good

---
+++ myproject/src/components/ex01_jsx/MyJsx002.js
@@ -0,0 +1,14 @@
+import { Fragment } from "react";
+
+const MyJsx002 = () => {
+  return (
+    //<Fragment>
+    <>
+      <p>start</p>
+      <span>!!!</span>
+    </>
+    //</Fragment>
+  ); //여러문장일때 반드시 소괄호 안
+};
+
+export default MyJsx002;
